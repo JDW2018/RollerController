a8f571d70555cbc3290c62aadbfdd91623c5cd7b
James Drew-Williams <james@jmtechnical.co.uk>
2020-12-14 23:28:50 +0000

    Timer Implementation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include "mbed.h"
+#include "timers.cpp"
 #include "types.h"
 
+std::list<IECTimer> Timers;
 
 // main() runs in its own thread in the OS
-int main()
-{
-    while (true) {
+int main() {
 
-    }
+  Init_Timers();
+
+  while (true) {
+    Update_Timers(Timers);
+  }
 }
-
--- types.h
+++ types.h
@@ -1,8 +1,1 @@
 
-
-struct Timer{
-    int Preset_Time;
-    int Elapsed_Time;
-    bool In;
-    bool Out; 
-};
